be051c8da31d44d3646f3bfa6d9a3042b85dab92
Nater <nater_nater@me.com>
2014-09-21 12:57:09 -0400

    began work on cell ranges and ability to merge cells

--- sample.js
+++ sample.js
@@ -1,4 +1,4 @@
-var xl = require('excel4node'),
+var xl = require('./lib/index.js'),
 http = require('http');
 
 function getAllMethods(object) {
@@ -18,14 +18,30 @@ myStyle.Font.Bold();
 myStyle.Font.Italics();
 myStyle.Font.Size(16);
 myStyle.Font.Family('Helvetica');
-myStyle.Font.Color('FF0000');
+myStyle.Font.Color('FFFF0000');
 myStyle.Number.Format("$#,##0.00;($#,##0.00);-");
+myStyle.Border(
+	{
+		left:{
+			style:'thin'
+		},
+		right:{
+			style:'thin'
+		},
+		top:{
+			style:'thin'
+		},
+		bottom:{
+			style:'thin'
+		}
+	}
+);
 
 var myStyle2 = wb.Style();
 myStyle2.Font.Size(18);
-myStyle2.Font.Color('ABABAB');
+myStyle2.Font.Color('FFABABAB');
 myStyle2.Fill.Pattern('solid');
-myStyle2.Fill.Color('FF54FF');
+myStyle2.Fill.Color('FFFF54FF');
 
 var myStyle3 = wb.Style();
 myStyle3.Border(
@@ -44,14 +60,16 @@ myStyle3.Border(
 		}
 	}
 );
-myStyle3.Font.Color('222222');
+myStyle3.Font.Alignment.Vertical('top');
+myStyle3.Font.Color('FF222222');
 myStyle3.Number.Format("##%");
 
 
 var ws = wb.WorkSheet('my worksheet');
 var ws2 = wb.WorkSheet('my 2nd worksheet');
+var ws3 = wb.WorkSheet('my 3rd worksheet');
 
-var img = ws.Image('sampleFiles/image1.png');
+//var img = ws.Image('sampleFiles/image1.png');
 var img2 = ws2.Image('sampleFiles/image2.jpg').Position(4,4,500000,500000);
 
 ws.Row(1).Height(60);
@@ -63,6 +81,10 @@ ws.Cell(1,4).String('newValue');
 ws.Cell(1,5).String('newValue');
 ws.Cell(2,5).String('2ndValue');
 ws.Cell(2,1).Number(100).Style(myStyle);
+ws.Cell(23,1,24,4).String('abc');
+ws.Cell(25,1,26,4,true).String('something merged');
+ws.Cell(25,1,26,4,true).Style(myStyle3);
+
 ws2.Cell(1,4).String('cell data');
 ws2.Cell(2,1).Number(5);
 ws2.Cell(2,1).Style(myStyle);
