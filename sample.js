2fe3cce262753c535436bf74667162e72a0a8fa2
Nater Jorde <nater@seas.harvard.edu>
2014-10-09 10:33:25 -0400

    fixed some bugs on applying styles to cell ranges began work on more useful example began work on allowing for single change to format of cell or cellrange.

--- sample.js
+++ sample.js
@@ -38,11 +38,10 @@ myStyle.Border(
 );
 
 var myStyle2 = wb.Style();
-myStyle2.Font.Size(18);
+myStyle2.Font.Size(16);
 myStyle2.Font.Color('FFABABAB');
 myStyle2.Fill.Pattern('solid');
-myStyle2.Fill.Color('FFFF54FF');
-
+myStyle2.Fill.Color('FF000000');
 var myStyle3 = wb.Style();
 myStyle3.Border(
 	{
@@ -61,29 +60,89 @@ myStyle3.Border(
 	}
 );
 myStyle3.Font.Alignment.Vertical('top');
+myStyle3.Font.Alignment.Horizontal('left');
+myStyle3.Font.WrapText(true);
 myStyle3.Font.Color('FF222222');
-myStyle3.Number.Format("##%");
 
+var myStyle4 = wb.Style();
+myStyle4.Border(
+	{
+		left:{
+			style:'thin'
+		},
+		right:{
+			style:'thin'
+		},
+		top:{
+			style:'thin'
+		},
+		bottom:{
+			style:'thin'
+		}
+	}
+);
+myStyle4.Font.Alignment.Horizontal('right');
+myStyle4.Font.Color('FF222222');
 
-var ws = wb.WorkSheet('my worksheet');
+var ws = wb.WorkSheet('Invoice');
 var ws2 = wb.WorkSheet('my 2nd worksheet');
 var ws3 = wb.WorkSheet('my 3rd worksheet');
 
-//var img = ws.Image('sampleFiles/image1.png');
+/*
+	Code to generate sample invoice
+*/
+
+ws.Row(1).Height(140);
+ws.Cell(1,1,2,6,true);
+ws.Image('sampleFiles/image1.png').Position(1,1,0,0);
+ws.Row(3).Height(50);
+ws.Cell(3,1,3,6,true).Style(myStyle3).String('Harvard School of Engineering and Applied Sciences\r\n29 Oxford St\r\nCambridge MA 02138');
+ws.Cell(4,1,15,6).Style(myStyle3);
+ws.Cell(4,1,4,6).Style(myStyle2);
+ws.Cell(4,1).String('Item');
+ws.Cell(4,2).String('Quantity');
+ws.Cell(4,3).String('Price/Unit');
+ws.Cell(4,6).String('Subtotal');
+
+var columnDefinitions = {
+	item:1,
+	quantity:2,
+	cost:3,
+	total:6
+}
+
+var invoiceItems = [
+	{
+		item: 'Soft Robot',
+		quantity: 5,
+		costPerUnit: 250.25
+	},
+	{
+		item: 'Quantum Transistor',
+		quantity:5,
+		costPerUnit:500
+	},
+	{
+		item:'Mountain Water Well',
+		quantity:2,
+		costPerUnit:50
+	}
+]
+
+var curRow = 5;
+invoiceItems.forEach(function(i){
+	ws.Cell(curRow,columnDefinitions.item).String(i.item);
+	ws.Cell(curRow,columnDefinitions.quantity).Number(i.quantity);
+	ws.Cell(curRow,columnDefinitions.cost).Number(i.costPerUnit);
+	ws.Cell(curRow,columnDefinitions.total).Formula(columnDefinitions.quantity.toExcelAlpha()+curRow+"*"+columnDefinitions.cost.toExcelAlpha()+curRow).Format.Number("$#,#00.00");
+	curRow+=1;
+});
+
+ws.Cell(16,1,16,5,true).Style(myStyle4).String('Total');
+ws.Cell(16,6).Style(myStyle4).Formula('SUM('+columnDefinitions.total.toExcelAlpha()+'5:'+columnDefinitions.total.toExcelAlpha()+'15)').Format.Number("$#,#00.00");
+
+
 var img2 = ws2.Image('sampleFiles/image2.jpg').Position(4,4,500000,500000);
-
-ws.Row(1).Height(60);
-ws.Column(1).Width(120);
-ws.Cell(1,1).String('Cell A1').Style(myStyle2);
-ws.Cell(1,2).String('Cell B1');
-ws.Cell(1,3).String('newValue');
-ws.Cell(1,4).String('newValue');
-ws.Cell(1,5).String('newValue');
-ws.Cell(2,5).String('2ndValue');
-ws.Cell(2,1).Number(100).Style(myStyle);
-ws.Cell(23,1,24,4).String('abc');
-ws.Cell(25,1,26,4,true).String('something merged').Style(myStyle3);
-
 ws2.Cell(1,4).String('cell data');
 ws2.Cell(2,1).Number(5);
 ws2.Cell(2,1).Style(myStyle);
