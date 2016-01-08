c609f946864add284535e08f3df64f83adca1b4b
Chadwick Dahlquist <chadwick@bugeats.net>
2016-01-07 21:09:13 -0800

    delint style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
     'rules': {
+        // TODO add a logger lib to the project
+        // 'no-console': 2
         'brace-style': [2, '1tbs'],
         'camelcase': 1,
         'comma-dangle': [2, 'never'],
@@ -10,8 +12,6 @@ module.exports = {
         'key-spacing': [2, { 'beforeColon': false, 'afterColon': true }],
         'linebreak-style': [2, 'unix'],
         'no-console': 0,
-        // TODO add a logger lib to the project
-        // 'no-console': 2
         'no-redeclare': 0,
         'no-underscore-dangle': 0,
         'no-unused-vars': 0,
@@ -19,8 +19,10 @@ module.exports = {
         'quotes': [2, 'single'],
         'semi': [2, 'always'],
         'semi-spacing': [2, { 'before': false, 'after': true }],
+        'space-after-keywords': 2,
         'space-before-blocks': [2, 'always'],
         'space-before-function-paren': [2, { 'anonymous': 'always', 'named': 'never' }],
+        'space-before-keywords': 2,
         'space-in-parens': [2, 'never'],
         'space-infix-ops': 2,
         'strict': 0
