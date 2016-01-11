77c888f8becee79ad7b8586a391032b2b5c4ad37
Chadwick Dahlquist <chadwick@bugeats.net>
2016-01-11 15:25:45 -0800

    Bump cell style coverage

--- tests/cell.test.js
+++ tests/cell.test.js
@@ -13,3 +13,12 @@ test('Cell coverage', function (t) {
     var cell = makeCell();
     t.ok(cell);
 });
+
+test('Cell takes a Style object', function (t) {
+    t.plan(1);
+    var wb = new xl.WorkBook();
+    var ws = wb.WorkSheet('test');
+    var cell = ws.Cell(1, 1);
+    var style = wb.Style();
+    t.ok(cell.Style(style));
+});
