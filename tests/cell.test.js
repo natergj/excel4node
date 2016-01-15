e11f720cf35ced20a7262aafb1c9d40038c838d9
dgofman <dgofman@gmail.com>
2016-01-14 20:18:57 -0800

    Issue #54

--- tests/cell.test.js
+++ tests/cell.test.js
@@ -22,3 +22,26 @@ test('Cell takes a Style object', function (t) {
     var style = wb.Style();
     t.ok(cell.Style(style));
 });
+
+test('Cell takes a Style object', function (t) {
+    t.plan(1);
+    var wb = new xl.WorkBook();
+    var ws = wb.WorkSheet('test');
+    var cell = ws.Cell(1, 1);
+    var content = [{
+            bold:true,
+            underline: true,
+            italic: true,
+            color: 'FF0000',
+            size: 18,
+            value: 'Hello'
+        }, ' World!', {color: '000000'},  ' All', ' these', ' strings', ' are', ' black', 
+                   {
+                      color: '0000FF',
+                      value: ', but'
+                   },  ' now', ' are', ' blue' ];
+
+    cell.Complex(content);
+
+    t.equal(JSON.stringify(wb.workbook.sharedStrings[0]), JSON.stringify(content), 'validating complex text');
+});
