b9858b5e2312e359d141471715cc8ae987f02a84
Nater Jorde <nater@iamnater.com>
2016-04-03 18:32:44 -0400

    set worksheet to allow for emoji characters

--- source/lib/worksheet/builder.js
+++ source/lib/worksheet/builder.js
@@ -435,7 +435,9 @@ let sheetXML = (ws) => {
                 'version': '1.0', 
                 'encoding': 'UTF-8', 
                 'standalone': true
-            }
+            },
+            { pubID: null, sysID: null },
+            { allowSurrogateChars: true }
         )
         .att('mc:Ignorable', 'x14ac')
         .att('xmlns', 'http://schemas.openxmlformats.org/spreadsheetml/2006/main')
