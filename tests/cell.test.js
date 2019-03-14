d01765f5556c071b85e97c16d00ac8c1a7e387bf
Ivan Rogov <xlazex@gmail.com>
2019-03-14 16:57:03 +0300

    test

--- tests/cell.test.js
+++ tests/cell.test.js
@@ -1,6 +1,6 @@
 const test = require('tape');
 const DOMParser = require('xmldom').DOMParser;
-const xl = require('../distribution/index');
+const xl = require('../source/index');
 
 test('Cell coverage', (t) => {
     t.plan(1);
@@ -86,4 +86,27 @@ test('Add Comment to cell', (t) => {
         t.ok(testComment.textContent === 'My test comment', 'Verify comment text is correct');
         t.end()
     });
+});
+
+test('Complex String value should NOT be rewritten from Style', (t) => {
+    const wb = new xl.Workbook();
+    const ws = wb.addWorksheet('test');
+    const arr = [ '1', '2', '3' ];
+    const style = {
+        bold: true,
+        underline: true,
+        italics: false,
+        color: '000000'
+    };
+    for (const i in arr) {
+        const el = arr[i];
+        ws.cell(1, parseInt(i)+1)
+            .string([ { ...style, value: el } ]);
+    }
+    const cells = ws.cells;
+    const values = Object.values(cells).map( e => wb.sharedStrings[e.v][0].value||wb.sharedStrings[e.v] );
+    t.ok(values[0]==arr[0], '1 cell value equals 1 array value');
+    t.ok(values[1]==arr[1], '2 cell value equals 2 array value');
+    t.ok(values[2]==arr[2], '3 cell value equals 3 array value');
+    t.end();
 });
